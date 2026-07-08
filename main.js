1c8e85cbfe4f2edaef3a5fdf18d32fca146eeff7
Mithun Yadav <mithun.yadav@webskitters.com>
2026-07-08 20:09:42 +0530

    feat: add folders and env

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 import express from "express";
+import "dotenv/config";
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello bro!");
 });
 
-app.listen("3000", () => {
-  console.log("Port is listening at 3000");
+app.listen(PORT, () => {
+  console.log(`PORT is listening at ${PORT}`);
 });
