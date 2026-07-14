5de7131a15cf4e0e16aab524dfb53c3421fb42cb
Mithun Yadav <mithun.yadav@webskitters.com>
2026-07-14 19:30:21 +0530

    fix: model syntax errors

--- model/auth.model.js
+++ model/auth.model.js
@@ -1,34 +1,36 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name: {
-    required: true,
-    type: string,
-    trim: true,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      required: true,
+      type: String,
+      trim: true,
+    },
+    email: {
+      required: true,
+      type: String,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      required: true,
+      type: String,
+      select: false,
+      minlength: 6,
+    },
+    resetToken: {
+      type: String,
+      default: null,
+    },
+    resetTokenExpire: {
+      type: String,
+      default: null,
+    },
   },
-  email: {
-    required: true,
-    type: string,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
-  password: {
-    required: true,
-    type: string,
-    select: false,
-    minlength: 6,
-  },
-  resetToken: {
-    type: string,
-    default: null,
-  },
-  resetTokenExpire: {
-    type: string,
-    default: null,
-  },
-  timestamp: true,
-});
+  { timestamps: true },
+);
 
 const User = mongoose.model("User", userSchema);
 export default User;
