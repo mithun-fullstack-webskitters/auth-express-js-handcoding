4c85ccc43ecce50a467b1d0461825ca4ef4a4210
Mithun Yadav <mithun.yadav@webskitters.com>
2026-07-15 14:40:00 +0530

    feat: profile get api work

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,16 +1,17 @@
 import User from "../model/auth.model.js";
 import jwt from "jsonwebtoken";
 
-const protect = (req, res, next) => {
+const protect = async (req, res, next) => {
+  console.log(req.headers.authorization, "token@@@");
   try {
     let token;
     if (
       req.headers.authorization &&
-      req.headers.authorization.startswith("Bearer")
+      req.headers.authorization.startsWith("Bearer")
     ) {
       token = req.headers.authorization.split(" ")[1];
     }
-    console.log(token, "token");
+    console.log(req.headers.authorization, token, "token@@@");
     if (!token) {
       return res.status(401).json({
         success: false,
@@ -18,19 +19,21 @@ const protect = (req, res, next) => {
       });
     }
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    if (!decode) {
+    if (!decoded) {
       return res.status(401).json({
         success: false,
         message: "Invalid token!",
       });
     }
-    const user = User.findById(decoded.id).select("-password");
+
+    const user = await User.findById(decoded.id).select("-password");
+
     req.user = user;
     next();
   } catch (error) {
     return res.status(401).json({
       success: false,
-      message: "Invalid or expired token",
+      message: error.message,
     });
   }
 };
