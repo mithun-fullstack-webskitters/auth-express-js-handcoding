04af04f15b96193e3baf3d0794e6b00fc40336ff
Mithun <mithun.yadav@webskitters.com>
2026-07-22 19:31:49 +0530

    test: testing api of northwest register agent

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import authRoute from "./route/auth.route.js";
 import cors from "cors";
+import crypto from "crypto-js";
+import sign from "jwt-encode";
 
 connectDB();
 dotenv.config();
@@ -13,6 +15,23 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+const access_key = "a7ac3cfc46eb2ba0258e047fefead62a637b8de06653acf06f671aa4eb0ef6bfdd9ffbe7d8da1ced";
+const secret_key = "a8c9fb0264a07152b1f7490cad5289e2e13a697420499c45dce838eb6f82b5454cff6fbf8675bae2";
+
+let headers = {
+  access_key: access_key,
+}
+
+let payload = {
+  path: '/callbacks/0af126b0-e4cc-41d8-b8b0-d0f7423ae29a',
+  content: crypto.SHA256().toString(crypto.enc.Hex),
+}
+
+let token = sign(payload, secret_key, headers)
+
+console.log(token);
+
 
 app.get("/", (req, res) => {
   res.json({
